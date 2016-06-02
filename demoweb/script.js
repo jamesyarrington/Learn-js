9737482d9d092fd3d869872e34663dd54df6ac5d
Yarrington <jyarrington@radiantblue.local>
2016-06-02 12:41:30 -0400

    Added style sheet, js, and customer page

---
+++ demoweb/script.js
@@ -0,0 +1,11 @@
+document.getElementById("foot01").innerHTML =
+	"<p>&copy;  " + 
+	new Date().getFullYear() + 
+	" W3Schools. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+	"<u1 id='menu'>" +
+	"<li><a href='index.html'>Home</a></li>" +
+	"<li><a href='customers.html'>Data</a></li>" +
+	"<li><a href='about.html'>About</a></li>" +
+	"</ul>";
