af04c0ed311858c91e001603c578d06f1ba0d814
Yarrington <jyarrington@radiantblue.local>
2016-06-06 16:38:23 -0400

    broke website, but karma test working!

--- demoweb/script.js
+++ demoweb/script.js
@@ -1,26 +1,17 @@
-generateFooter = function() {
+exports.generateFooter = function() {
 	return "<p>&copy;  " + 
 		new Date().getFullYear() + 
 		" W3Schools. All rights reserved.</p>";
-}
+};
 
-generateNav = function() {
-	return "<u1 id='menu'>" +
-		"<li><a href='index.html'>Home</a></li>" +
-		"<li><a href='customers.html'>Data</a></li>" +
-		"<li><a href='about.html'>About</a></li>" +
-		"</ul>";
-}
+exports.generateNav = function() {
+	return '<ul id="menu">' +
+		'<li><a href="index.html">Home</a></li>' +
+		'<li><a href="customers.html">Data</a></li>' +
+		'<li><a href="about.html">About</a></li>' +
+		'</ul>';
+};
 
-replaceHTML = function(id, newHTML) {
+exports.replaceHTML = function(id, newHTML) {
 	document.getElementById(id).innerHTML = newHTML;
-}
-allFunctions = function() {
-	this.generateFooter = new generateFooter;
-	this.generateNav = new generateNav;
-	this.replaceHTML = new replaceHTML;
-}
-
-module.exports = {
-	generateFooter, generateNav, replaceHTML
 };
