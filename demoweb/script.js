b6cac1a871044d3c437a2ca0b4cc6d9c6718ba81
Yarrington <jyarrington@radiantblue.local>
2016-06-06 08:34:29 -0400

    Started work on Jasmine testing.

--- demoweb/script.js
+++ demoweb/script.js
@@ -1,11 +1,26 @@
-document.getElementById("foot01").innerHTML =
-	"<p>&copy;  " + 
-	new Date().getFullYear() + 
-	" W3Schools. All rights reserved.</p>";
+generateFooter = function() {
+	return "<p>&copy;  " + 
+		new Date().getFullYear() + 
+		" W3Schools. All rights reserved.</p>";
+}
 
-document.getElementById("nav01").innerHTML =
-	"<u1 id='menu'>" +
-	"<li><a href='index.html'>Home</a></li>" +
-	"<li><a href='customers.html'>Data</a></li>" +
-	"<li><a href='about.html'>About</a></li>" +
-	"</ul>";
+generateNav = function() {
+	return "<u1 id='menu'>" +
+		"<li><a href='index.html'>Home</a></li>" +
+		"<li><a href='customers.html'>Data</a></li>" +
+		"<li><a href='about.html'>About</a></li>" +
+		"</ul>";
+}
+
+replaceHTML = function(id, newHTML) {
+	document.getElementById(id).innerHTML = newHTML;
+}
+allFunctions = function() {
+	this.generateFooter = new generateFooter;
+	this.generateNav = new generateNav;
+	this.replaceHTML = new replaceHTML;
+}
+
+module.exports = {
+	generateFooter, generateNav, replaceHTML
+};
